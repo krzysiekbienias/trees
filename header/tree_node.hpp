2136e45410927217d0f70a29bb8cc44919eafb57
krzysiekbienias <krzysiek.bienias.kr@gmail.com>
2026-03-21 22:31:54 +0100

    feat (domain): add Node with tree node with additional pointer that points to the parent of the node

--- header/tree_node.hpp
+++ header/tree_node.hpp
@@ -6,6 +6,20 @@ public:
     TreeNode* left=nullptr;
     TreeNode* right=nullptr;
 
-    TreeNode(int val):m_val(val),left(nullptr),right(nullptr){};
+    explicit TreeNode();
+    explicit TreeNode(int val):m_val(val),left(nullptr),right(nullptr){};
 };
+
+class  TreeNodeWithParent {
+public:
+    int m_val;
+    TreeNode* left=nullptr;
+    TreeNode* right=nullptr;
+    TreeNode* parent=nullptr;
+
+    explicit TreeNodeWithParent();
+    explicit TreeNodeWithParent(int val):m_val(val),left(nullptr),right(nullptr),parent(nullptr){};
+};
+
+
 
